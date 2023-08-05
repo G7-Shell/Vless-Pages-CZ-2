10ee924d22091372de1b1f1cc297e67c419ae68a
✳ Adolf Commerce <128338681+adolf-commerce@users.noreply.github.com>
2023-08-05 19:42:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'bc8c8e7e-7800-435b-9e70-ae80c9ce15d2';
 
-let proxyIP = '195.85.23.8';
+let proxyIP = '188.64.13.140';
 
 let dohURL = 'https://dns.google/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
